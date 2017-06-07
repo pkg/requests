67191f091f52833624fba68aa9ea10efea7b3097
Dave Cheney <dave@cheney.net>
2017-06-07 15:50:33 +1000

    added Body.JSON example

--- example_test.go
+++ example_test.go
@@ -2,6 +2,7 @@ package requests_test
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/pkg/requests"
 )
@@ -11,7 +12,30 @@ func ExampleClient_Get() {
 
 	resp, err := c.Get("https://www.example.com")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	fmt.Println(resp.Request.Method, resp.Request.URL, resp.Status.Code)
+}
+
+func ExampleBody_JSON() {
+	var c requests.Client
+
+	resp, err := c.Get("https://frinkiac.com/api/search?q=burn+that+seat")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Close()
+
+	if !resp.IsSuccess() {
+		log.Fatalf("%s: expected 200, got %v", resp.Request.URL, resp.Status)
+	}
+
+	var results []struct {
+		Id        int    `json:"Id"`
+		Episode   string `json:"Episode"`
+		Timestamp int    `json:"Timestamp"`
+	}
+
+	err = resp.JSON(&results)
+	fmt.Printf("%#v\n%v", results, err)
 }
